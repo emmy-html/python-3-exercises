50f0fd18c713f5dc101896fdcea96602a8e36ea5
Emmy <60683173+emmy-html@users.noreply.github.com>
2021-02-12 23:22:37 -0600

    Initial commit with initials script

---
+++ initials.py
@@ -0,0 +1,13 @@
+# Emmy Jordan
+# I love sushi but it makes my stomach hurt.
+e = "E "
+j = "J "
+double_space = "  "
+single_space = " "
+print(e * 5 + double_space + j *5)
+print(e + (single_space * 14) + j)
+print(e + (single_space * 14) + j)
+print(e * 3 + (single_space * 10) + j)
+print(e + ((single_space * 10) + j) + ((single_space * 2) + j))
+print(e + ((single_space * 10) + j) + ((single_space * 2) + j))
+print(e * 5 + ((single_space * 4) + j) + j)
